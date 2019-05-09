7ae826831c8950c48223ef8a5746c4e2cac79948
Jiaqi <jqcui@ucdavis.edu>
2019-05-09 22:31:41 +0000

    added by jiaqi

--- BLAST.app/server.R
+++ BLAST.app/server.R
@@ -6,21 +6,46 @@
 # 
 #    http://shiny.rstudio.com/
 #
-
+library(tidyverse)
+worm_plant <- read_tsv("~/Downloads/worm_vs_plant.blastout_v2.1.gz",col_names=FALSE)
+colnames(worm_plant) <- c("query_id",
+                          "subject_id",
+                          "pct_ident",
+                          "len",
+                          "mis",
+                          "gaps",
+                          "qb",
+                          "qe",
+                          "sb",
+                          "se",
+                          "E",
+                          "Score")
 library(shiny)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
   
+  # Expression that generates a boxplot. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  output$scatterPlot <- renderPlot({
+    
+    # set up the plot
+    pl <- ggplot(data = worm_plant,
+                 #Use aes_string below so that input$trait is interpreted
+                 #correctly.  The other variables need to be quoted
+                 aes_string(x="len",
+                            y="Score",
+                            color = input$trait
+                 )
+    )
+    
+    # draw the boxplot for the specified trait
+    pl + geom_point()
+  })
 })
